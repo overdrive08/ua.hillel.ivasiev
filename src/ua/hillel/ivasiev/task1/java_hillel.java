53c956861c3b9c1f97b2c501612022601963bfd0
Serega <ivss.engineering@gmail.com>
2018-11-22 19:02:02 +0200

    Task_2

--- src/ua/hillel/ivasiev/task1/java_hillel.java
+++ src/ua/hillel/ivasiev/task1/java_hillel.java
@@ -1,4 +1,5 @@
 package ua.hillel.ivasiev.task1;
+
 import java.util.Scanner;
 import java.lang.String;
 
@@ -10,26 +11,29 @@ public class java_hillel {
         int num = user.nextInt();
         System.out.println("N = " + num);
 
-        if (num < 0){
+        if (num < 0) {
             System.out.println("Error");
             System.out.println("\n");
         } else {
             String binary = Integer.toBinaryString(num);
             System.out.println("Convert to binary is = " + binary);
 
-            int a =Integer.parseInt(binary);
-            int count = 0;
-            while (a!=0){
-                a %=2;
-                if (a==1);
-                count++;
-                a = a/2;
-                //if (a==0)
-                   //return;
-            }
-
-            System.out.println("Number of units = " + count);
+            int a = Integer.parseInt(binary,2);
+            int sumOfBinaryDigits = sumOfBinaryDigits(a);
+            System.out.println("sumOfBinaryDigits="+sumOfBinaryDigits);
 
         }
     }
+
+    private static int sumOfBinaryDigits(int a) {
+        int count = 0;
+        int b = 0;
+        while (a != 0) {
+            b = a % 2;
+            a = a / 2;
+            count += b;
+        }
+        return count;
+    }
 }
+

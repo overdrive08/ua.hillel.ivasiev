e7d42c290297353ada2daf1724f763d4b20be2fa
Serega <ivss.engineering@gmail.com>
2018-11-22 13:17:15 +0200

    Колличество символов с строке task1

--- src/ua/hillel/ivasiev/task1/java_hillel.java
+++ src/ua/hillel/ivasiev/task1/java_hillel.java
@@ -1,56 +1,21 @@
 package ua.hillel.ivasiev.task1;
 
-import java.util.Random;
 import java.util.Scanner;
 
 public class java_hillel {
     static Scanner scanner;
     public static void main(String[] args) {
-    /*    var a = 2.0;
-        var b = 1.1;
-        var c = 8;
-        var d = 2;
 
-        a = a + b;
-        b = a - b;
-        a = a - b;
-
-        c = c * d;
-        d = c / d;
-        c = c / d;
-
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
-        System.out.println("c = " + c);
-        System.out.println("d = " + d);
-
-
-        int[][] mas2d = new int[10][10];
-        Random rand = new Random();
-
-        for (int i = 0; i < 10; i++)
-            for (int j = 0; j < 10; j++)
-                mas2d[i][j] = rand.nextInt(100);
-
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++)
-                System.out.print(mas2d[i][j] + "\t" + " ");
-            System.out.println();
-        }*/
-        String name = new String  ("*        * * *\n" +
+        String name = "*        * * *\n" +
                       "*      *\n" +
                       "*      *\n" +
                       "*        * *\n" +
                       "*            *\n" +
                       "*            *\n" +
-                      "*      * * *");
-        int cnt = 0;
+                      "*      * * *";
         int sim1 = 0;
         int sim2 = 0;
         for (char i = 0; i < name.length(); i++) {
-            char c = name.charAt(i);
-            //System.out.println(c);
-            cnt++;
                 if (name.charAt(i) == ' '){
                     sim1++;
                 }
@@ -60,7 +25,6 @@ public class java_hillel {
         }
         System.out.println("Пробелов в строке = " + sim1);
         System.out.println("Символов * в строке = " + sim2);
-        //System.out.println(cnt);
         System.out.println(name);
     }
 }

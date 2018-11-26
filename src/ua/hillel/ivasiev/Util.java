bb433aa70605d598fbce9f7af79373ed4f1ae544
Serega <ivss.engineering@gmail.com>
2018-11-26 06:26:14 +0200

    unit

---
+++ src/ua/hillel/ivasiev/Util.java
@@ -0,0 +1,8 @@
+package ua.hillel.ivasiev;
+
+public class Util {
+    public static boolean checkPalindrom(String checkStr){
+        System.out.println("Checking palindrom :" + checkStr);
+        return true;
+    }
+}

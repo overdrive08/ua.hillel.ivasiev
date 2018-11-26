ccbea22dbe41959957379d9716213c618b779951
Serega <ivss.engineering@gmail.com>
2018-11-26 05:51:17 +0200

    init

---
+++ src/ua/hillel/ivasiev/Lesson3_3.java
@@ -0,0 +1,12 @@
+/*3. Реализавать в ItelliJ IDEA. Создать две ветки репозитория. В одной реализовать метод проверки является ли строка палидромом.
+запаковать все в jar файл. Во ворой ветке "пристыковать" полученный в первой ветке jar файл и вызвать метод и вывести
+результат. "Смёржить" эти две ветки.*/
+
+package ua.hillel.ivasiev;
+
+public class Lesson3_3 {
+    public static boolean checkPalidrom(String checkStr) {
+        System.out.println(" Checking palidrom :" + checkStr);
+        return true;
+    }
+}

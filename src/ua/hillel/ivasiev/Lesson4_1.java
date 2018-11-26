ff0b8b760bf0fbeedb5fdbad26ce794e7ce7b991
Serega <ivss.engineering@gmail.com>
2018-11-26 18:43:35 +0200

    Fibonacchi

--- src/ua/hillel/ivasiev/Lesson4_1.java
+++ src/ua/hillel/ivasiev/Lesson4_1.java
@@ -1,24 +1,18 @@
-/*1. Пользователь вводит отсортированный массив чисел и еще одно число. Требуется сформировать новый отсортированый массив
-котрый соостоит из исходного и нового числа.*/
-
 package ua.hillel.ivasiev;
 
 import java.util.Scanner;
 
 public class Lesson4_1 {
-    public static void main(String[] args) {
-        int n;
+    public static void main(String[] args){
+        System.out.println("Enter data array");
+        Scanner input = new Scanner(System.in);
+        int size = input.nextInt();
+        int array[] = new int[size];
 
-        Scanner s = new Scanner(System.in);
-        n = s.nextInt() + 2;
-        int fib [] = new int[n];
+        for (int i = 0; i < size; i++)
+            array [i] = input.nextInt();
 
-        fib [0] = 0;
-        fib [1] = 1;
+        System.out.println(" Input =" + array);
 
-        for (int i = 1; i < n-1; i++) {
-            fib[i + 1] = fib[i] + fib[i - 1];
-            System.out.print(fib[i] + " ");
-        }
     }
 }
---
+++ src/ua/hillel/ivasiev/Lesson4_4.java
@@ -0,0 +1,25 @@
+/*1. Пользователь вводит отсортированный массив чисел и еще одно число. Требуется сформировать новый отсортированый массив
+котрый соостоит из исходного и нового числа.*/
+
+package ua.hillel.ivasiev;
+
+import java.util.Scanner;
+
+public class Lesson4_4 {
+    public static void main(String[] args) {
+        int n;
+
+        System.out.println("Insert the number :");
+        Scanner s = new Scanner(System.in);
+        n = s.nextInt() + 2;
+        int fib [] = new int[n];
+
+        fib [0] = 0;
+        fib [1] = 1;
+
+        for (int i = 1; i < n-1; i++) {
+            fib[i + 1] = fib[i] + fib[i - 1];
+            System.out.print(fib[i] + " ");
+        }
+    }
+}

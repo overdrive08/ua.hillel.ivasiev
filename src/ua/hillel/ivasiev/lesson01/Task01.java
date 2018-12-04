91e933f31b636ba583b2f2e2a85be4752ee315d8
Serega <ivss.engineering@gmail.com>
2018-12-04 12:50:30 +0200

    функции в отдельном методе

--- src/ua/hillel/ivasiev/lesson01/Task01.java
+++ src/ua/hillel/ivasiev/lesson01/Task01.java
@@ -6,28 +6,57 @@ import java.util.Scanner;
 
 
 public class Task01 {
-        static Scanner scanner;
-        public static void main(String[] args) {
+    static Scanner scanner;
 
-            int[][] mas2d = new int[10][10];
-            Random rand = new Random();
+    public static void main(String[] args) {
 
-            for (int i = 0; i < 10; i++)
-                for (int j = 0; j < 10; j++)
-                    mas2d[i][j] = rand.nextInt(100);
+        /*int[][] mas2d = new int[10][10];
+        Random rand = new Random();
 
-            for (int i = 0; i < 10; i++) {
-                for (int j = 0; j < 10; j++)
-                    System.out.print(mas2d[i][j] + "\t" + " ");
-                System.out.println();
-            }
-            System.out.println("*        * * *\n" +
-                    "*      *\n" +
-                    "*      *\n" +
-                    "*        * *\n" +
-                    "*            *\n" +
-                    "*            *\n" +
-                    "*      * * *");
+        for (int i = 0; i < 10; i++)
+            for (int j = 0; j < 10; j++)
+                mas2d[i][j] = rand.nextInt(100);
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++)
+                System.out.print(mas2d[i][j] + "\t" + " ");
+            System.out.println();
         }
-    
+
+        System.out.println("*        * * *\n" +
+                "*      *\n" +
+                "*      *\n" +
+                "*        * *\n" +
+                "*            *\n" +
+                "*            *\n" +
+                "*      * * *");*/
+        test();
+        init();
+    }
+
+
+    private static void test() {
+        int[][] mas2d = new int[10][10];
+        Random rand = new Random();
+
+        for (int i = 0; i < 10; i++)
+            for (int j = 0; j < 10; j++)
+                mas2d[i][j] = rand.nextInt(100);
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++)
+                System.out.print(mas2d[i][j] + "\t" + " ");
+            System.out.println();
+        }
+    }
+
+    private static void init(){
+        System.out.println("*        * * *\n" +
+                "*      *\n" +
+                "*      *\n" +
+                "*        * *\n" +
+                "*            *\n" +
+                "*            *\n" +
+                "*      * * *");
+    }
 }

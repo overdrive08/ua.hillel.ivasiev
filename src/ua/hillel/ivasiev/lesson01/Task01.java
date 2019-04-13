65d1c2adb0b0afe48f7ba3b7f6b0f179c7bff9fe
Serega <ivss.engineering@gmail.com>
2019-04-13 19:48:42 +0300

    Figures of stars

--- src/ua/hillel/ivasiev/lesson01/Task01.java
+++ src/ua/hillel/ivasiev/lesson01/Task01.java
@@ -1,63 +1,92 @@
-/*1. Вывести рандомно массив 10х10 со значениями от 0-100 и свои инициалы*/
 package ua.hillel.ivasiev.lesson01;
-
-import java.util.Random;
-import java.util.Scanner;
-
-
+import java.awt.Graphics;
 
 public class Task01 {
-    static Scanner scanner;
-
     public static void main(String[] args) {
+        Task01 printer = new Task01();
 
-        /*int[][] mas2d = new int[10][10];
-        Random rand = new Random();
-
-        for (int i = 0; i < 10; i++)
-            for (int j = 0; j < 10; j++)
-                mas2d[i][j] = rand.nextInt(100);
-
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++)
-                System.out.print(mas2d[i][j] + "\t" + " ");
-            System.out.println();
-        }
-
-        System.out.println("*        * * *\n" +
-                "*      *\n" +
-                "*      *\n" +
-                "*        * *\n" +
-                "*            *\n" +
-                "*            *\n" +
-                "*      * * *");*/
-        test();
-        init();
+        printSquare();
+        printTriangle();
+        printLeftTriagle();
+        printRightTriagle();
+        printRhumbus();
+        printChessBoard(); // что означает printer.printChessBoard(); ?
     }
 
+    private static void printSquare() {
+        for (int i = 0; i <= 0; i++) {
+            System.out.println("1.\n" + "********");
+            for (int j = 0; j <= 3; j++) {
+                System.out.print("*      *\n");
+            }
+            System.out.println("********");
+        }
+    }
 
-    private static void test() {
-        int[][] mas2d = new int[10][10];
-        Random rand = new Random();
+    private static void printTriangle() {
+        System.out.println("2.");
 
-        for (int i = 0; i < 10; i++)
-            for (int j = 0; j < 10; j++)
-                mas2d[i][j] = rand.nextInt(100);
+        for (int i = 0; i < 4; i++)
+            System.out.println("   *******".substring(i, 4 + 2 * i)); // решение подсказал интернет)
 
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++)
-                System.out.print(mas2d[i][j] + "\t" + " ");
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5 - i; j++)
+                System.out.print(" ");
+            for (int k = 0; k < (2 * i + 1); k++)
+                System.out.print("*");
             System.out.println();
         }
     }
 
-    private static void init(){
-        System.out.println("*        * * *\n" +
-                "*      *\n" +
-                "*      *\n" +
-                "*        * *\n" +
-                "*            *\n" +
-                "*            *\n" +
-                "*      * * *");
+    private static void printLeftTriagle() {
+        int count = 6;
+        System.out.print("3.");
+
+        for (int i = 0; i <= count; i++) {
+            for (int j = 0; j < i; j++)
+                System.out.print("*");
+            System.out.println();
+        }
+    }
+
+    private static void printRightTriagle() {
+        System.out.println("4.");
+
+        for (int i = 0; i < 6; i++)
+            System.out.println("     *******".substring(i, 6 + i));
+    }
+
+    private static void printRhumbus() {           // пустой ромб из звезд пока не знаю как реализовать изучаю алгоритм брезенхема
+        System.out.println("5.");
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5 - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < (2 * i + 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < i + 2; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 8; k > (i * 2 + 1); k--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void printChessBoard() {
+        System.out.println("6.");
+
+        for (int i = 0; i <= 3; i++) {
+            System.out.println("********");
+            for (int j = 0; j <= 0; j++) {
+                System.out.print(" ********\n");
+            }
+        }
     }
 }

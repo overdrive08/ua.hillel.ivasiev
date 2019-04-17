4188f49904eb7e0b17e1eac3fac71ee7e50d8725
Serega <ivss.engineering@gmail.com>
2019-04-17 15:31:43 +0300

    task01

--- src/ua/hillel/ivasiev/lesson02/Task01.java
+++ src/ua/hillel/ivasiev/lesson02/Task01.java
@@ -1,11 +1,32 @@
-/*Вывести первые 5 элементов массива, у которого размер 10*/
 package ua.hillel.ivasiev.lesson02;
 
 public class Task01 {
-    public static void main(String[] args) {
-        int massive [] = new int [] {1,2,3,4,5,6,7,8,9,10};
-        for (int i = 0; i < 5; i++) {
-            System.out.print(massive[i] + " ");
-        }
+  public static void main(String[] args)
+  {
+    int from = 0;
+    int to = 2;
+    // изввлечь в отдельный метод и назвать его
+    // printMultiplicationTable(int from, int to)
+    printMultiplicationTable(from, to);
+  }
+
+  private static void printMultiplicationTable(int from, int to)
+  {
+    for (int i = 1; i < 10; i++)
+    {
+      // Извлечь в в отдельный метод и назвать его
+      // printTableForNumber(int number)
+      int number = 3;
+      printTableForNumber(number);
+      System.out.println();
     }
+  }
+
+  private static void printTableForNumber(int i)
+  {
+    for (int j = 1; j < 10; j++)
+    {
+      System.out.printf("%d * %d = %d\n", i, j, i * j);
+    }
+  }
 }

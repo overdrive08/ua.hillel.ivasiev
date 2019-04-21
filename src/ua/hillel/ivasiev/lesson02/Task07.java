d03659453bfdd6d947c6f8a0334a309626273038
Serega <ivss.engineering@gmail.com>
2019-04-21 22:39:51 +0300

    fixed

--- src/ua/hillel/ivasiev/lesson02/Task07.java
+++ src/ua/hillel/ivasiev/lesson02/Task07.java
@@ -1,18 +1,9 @@
-/*Найдите среднее арифметическое элементов массива*/
-
 package ua.hillel.ivasiev.lesson02;
 
-public class Task07 {
-    public static void main(String[] args) {
-        int massive [] = new int [] {1,2,3,4,5,6,7,8,9,10};
-        double average= 0;
-        int n = 0;
+public class Task07
+{
+  public static void main(String[] args)
+  {
 
-        for (int i = 0; i < massive.length; i++) {
-            average += massive[i];
-            n++;
-        }
-        average /=n;
-        System.out.print(average + " ");
-    }
+  }
 }
